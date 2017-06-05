0885cdb5ff3674899bf1349f75eb960a0847871c
Troy Kinsella <troy.kinsella@startmail.com>
2017-06-05 15:13:20 -0700

    Fix status repaint issue

--- bacon.go
+++ bacon.go
@@ -133,7 +133,7 @@ func (b *Bacon) printStatus(s *status, repaint bool) {
 	}
 
 	if !b.showOutput && repaint {
-		fmt.Print("\033[1A                                \033[1A")
+		fmt.Print("\033[1A                                \033[32D")
 	}
 
 	tpl.Execute(os.Stdout, vars)
